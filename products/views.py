392c297e78cab245327d7c665b897e46c469955c
KeyanBawazir <key42pc@gmail.com>
2020-09-21 20:12:58 +0300

    create first views and template

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+
+# Create your views here.
+def say_hi(request,name):
+    return render(request,'hi.html',{'name':name})
+def show_time(request):
+    now = timezone.now() 
+    return render(request, 'time.html',{'time': now})   
